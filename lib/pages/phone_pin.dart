f1e43fda22f542f44c4ddb9914c4e13ca15b6504
Camilo_Contreras-MFS <75485698+concamil-globant@users.noreply.github.com>
2022-03-09 19:13:30 -0300

    updated view

--- lib/pages/phone_pin.dart
+++ lib/pages/phone_pin.dart
@@ -1,14 +1,25 @@
 import 'package:flutter/material.dart';
 
-class PhonePinScreen extends StatelessWidget {
+class PhonePinScreen extends StatefulWidget {
    
   const PhonePinScreen({Key? key}) : super(key: key);
-  
+
+  @override
+  State<PhonePinScreen> createState() => _PhonePinScreenState();
+}
+
+class _PhonePinScreenState extends State<PhonePinScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-         child: Text('PhonePinScreen'),
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        margin: const EdgeInsets.only(top: 40.0, left: 20.0, right: 20.0),
+        child: Column(
+          children: const [
+            Text('Phone + Pin')
+          ],
+        ),
       ),
     );
   }
