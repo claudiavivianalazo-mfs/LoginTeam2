6ef0dedefd2ac066bc34f866d2f982da98e9e81c
Camilo_Contreras-MFS <75485698+concamil-globant@users.noreply.github.com>
2022-03-09 18:02:47 -0300

    phone + pin initial creation

---
+++ lib/pages/phone_pin.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PhonePinScreen extends StatelessWidget {
+   
+  const PhonePinScreen({Key? key}) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+         child: Text('PhonePinScreen'),
+      ),
+    );
+  }
+}
