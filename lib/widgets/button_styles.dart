65ca9afe97945327e9e29802dcf27cf3b15c44fe
nora-strasberg <nora.strasberg@millicom.com>
2022-03-10 11:31:28 -0300

    add reusable ButtonStyle for ElevatedButton

--- lib/widgets/button_styles.dart
+++ lib/widgets/button_styles.dart
@@ -1,22 +1,45 @@
 import 'package:flutter/material.dart';
 
-/// Example of usage:
+/// Usage:
+///
 /// TextButton.icon(
 ///      style: textButtonStyle,
 ///      onPressed: () => {},
 ///      icon: const Icon(
 ///            Icons.mail,),
 ///      label: const Text('Iniciar con correo electrónico')),
+///
+/// You can override TextStyle in your widget:
+///
+/// ElevatedButton(
+///      style: elevatedButtonStyle,
+///      onPressed: () => {},
+///      label: const Text('Iniciar con correo electrónico',
+///           style: TextStyle(fontWeight: FontWeight.normal)),
+///      ),
+///
+///
+///
 
 const primaryColor = Color(0xff363853);
 
 ButtonStyle textButtonStyle = TextButton.styleFrom(
   primary: primaryColor,
-  backgroundColor: Colors.white,
   side: const BorderSide(
     color: primaryColor,
     style: BorderStyle.solid,
   ),
+  padding: const EdgeInsets.symmetric(horizontal: 10),
+  shape: const RoundedRectangleBorder(
+    borderRadius: BorderRadius.all(
+      Radius.circular(10),
+    ),
+  ),
+  textStyle: const TextStyle(fontWeight: FontWeight.bold),
+);
+
+ButtonStyle elevatedButtonStyle = ElevatedButton.styleFrom(
+  primary: primaryColor,
   padding: const EdgeInsets.symmetric(horizontal: 10),
   shape: const RoundedRectangleBorder(
     borderRadius: BorderRadius.all(
